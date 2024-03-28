2c3583521e6818bb792b433769c48ee7033d7ca9
Erjona Haka <111374968+jonah05@users.noreply.github.com>
2024-03-28 17:58:15 +0100

    allFactors.java

---
+++ allFactors.java
@@ -0,0 +1,16 @@
+public class FactorPrinter {
+ 
+    public static void printFactors(int number) {
+        
+        if (number < 1) {
+            System.out.println("Invalid Value");
+            return;
+        }
+ 
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
